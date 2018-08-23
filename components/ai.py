6d0c7eb93dacbaaa48eb901676cba94a2ef32c94
Jason Gilbert <0jason.gilbert@gmail.com>
2018-08-22 21:59:53 -0300

    Reinstate A* pathing

--- components/ai.py
+++ components/ai.py
@@ -8,7 +8,8 @@ class BasicMonster:
         if libtcod.map_is_in_fov(fov_map, monster.x, monster.y):
 
             if monster.distance_to(target) >= 2:
-                monster.move_towards(target.x, target.y, game_map, entities)
+                # monster.move_towards(target.x, target.y, game_map, entities) # This is less intense, but less useful.
+                monster.move_astar(target, entities, game_map)
 
             elif target.fighter.hp > 0:
                 attack_results = monster.fighter.attack(target)
